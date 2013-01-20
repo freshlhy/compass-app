d7b678351f1681cb7137d6a9def36457716d444c
liuhuyu <735077491@qq.com>
2013-01-21 00:21:40 +0800

    first commit with layout and spr logo

---
+++ app/controllers/index_page_controller.rb
@@ -0,0 +1,10 @@
+class IndexPageController < ApplicationController
+  def index
+
+  end
+
+  def box
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassApp::Application.config.secret_token = '67e6e6838e13b5f8bae8c074b67d49a1a5096a444cd501aa7b8a9e23a74b2dce942a0265fd73792ce7196b0a0487baee02e2ec731b1fe7ee2a8a9a27c2331d05'
